008862103efcfc283e8df18d53bdd73d950260bb
Kareem <knassar89@gmail.com>
2011-04-10 16:37:46 -0700

    pet model

---
+++ Amigotchi/Amigotchi/Pet.h
@@ -0,0 +1,28 @@
+//
+//  Pet.h
+//  Amigotchi
+//
+//  Created by Kareem Nassar on 4/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Pet :  CCNode{
+    CCSprite        *bodySprite;
+    CCSprite        *headSprite;
+    CCSprite        *limbSprite;
+    
+    NSString        *name;
+    
+    int             hunger;
+    int             attention;
+    int             health;
+    int             age;
+    int             strength;
+    int             bathroom;
+    
+}
+
+@end
---
+++ Amigotchi/Amigotchi/Pet.m
@@ -0,0 +1,14 @@
+//
+//  Pet.m
+//  Amigotchi
+//
+//  Created by Kareem Nassar on 4/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Pet.h"
+
+
+@implementation Pet
+
+@end
