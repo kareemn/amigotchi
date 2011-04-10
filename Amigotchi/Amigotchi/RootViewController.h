89f391d45f5f18a77f03d3562d45c8362401dec6
Kareem <knassar89@gmail.com>
2011-04-10 15:50:22 -0700

    Amigotchi commit

---
+++ Amigotchi/Amigotchi/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Amigotchi
+//
+//  Created by Kareem Nassar on 4/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
