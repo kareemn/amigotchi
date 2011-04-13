14bcb531c1ae2581f6bba0abef720285e2c899f7
Elliott Kipper <Kip@Toppington.local>
2011-04-12 18:27:27 -0700

    Trying to make MVC penises

---
+++ Amigotchi/Amigotchi/AmigoPetView.h
@@ -0,0 +1,17 @@
+//
+//  AmigoPetView.h
+//  Amigotchi
+//
+//  Created by Elliott Kipper on 4/12/11.
+//  Copyright 2011 kipgfx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+
+@interface AmigoPetView : CCNode {
+    
+}
+
+@end
---
+++ Amigotchi/Amigotchi/AmigoPetView.m
@@ -0,0 +1,14 @@
+//
+//  AmigoPetView.m
+//  Amigotchi
+//
+//  Created by Elliott Kipper on 4/12/11.
+//  Copyright 2011 kipgfx. All rights reserved.
+//
+
+#import "AmigoPetView.h"
+
+
+@implementation AmigoPetView
+
+@end
