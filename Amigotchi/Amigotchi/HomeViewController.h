95fa8953359b8671c2b5268d45b495db21783afa
Elliott Kipper <Kip@pcp066489pcs.wireless.calpoly.edu>
2011-05-16 22:08:32 -0700

    t add Amio

---
+++ Amigotchi/Amigotchi/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Amigotchi
+//
+//  Created by Elliott Kipper on 5/16/11.
+//  Copyright 2011 kipgfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HomeViewController : UIViewController {
+    
+}
+
+@end
