68081616ed0ab1319c9251e7a70de1350abc1047
Elliott Kipper <Kip@pcp066656pcs.wireless.calpoly.edu>
2011-05-26 19:51:36 -0700

    AmigoAlertLayer

---
+++ Amigotchi/Amigotchi/AmigoAlertLayer.h
@@ -0,0 +1,16 @@
+//
+//  AmigoAlertLayer.h
+//  Amigotchi
+//
+//  Created by Elliott Kipper on 5/26/11.
+//  Copyright 2011 kipgfx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AmigoAlertLayer : CCLayer {
+    
+}
+
+@end
---
+++ Amigotchi/Amigotchi/AmigoAlertLayer.m
@@ -0,0 +1,14 @@
+//
+//  AmigoAlertLayer.m
+//  Amigotchi
+//
+//  Created by Elliott Kipper on 5/26/11.
+//  Copyright 2011 kipgfx. All rights reserved.
+//
+
+#import "AmigoAlertLayer.h"
+
+
+@implementation AmigoAlertLayer
+
+@end
